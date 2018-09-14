12164ba3b398c38a3c5aaa4598362906b50fab31
okan yesiloglu <myesiloglu0@gmail.com>
2018-09-15 01:37:19 +0300

    versiyon 1.0

---
+++ sets.py
@@ -0,0 +1,10 @@
+# w3school'dan takip ettiğim python derslerinin sets konusu örnekleri.
+
+new_sets={"Java","c#","c++","c","ruby"}#sets küme parantezi içerisinde tanımlanır.
+print(new_sets)
+#sets sayılamayan ve indexlenemez
+for y in new_sets:
+  print(y)
+#sets'i bir döngü içerisinde yazdırabiliriz.
+print("c#" in new_sets)#setin içindeki veriyi in komutuyla arayabiliriz
+#eğer değer içinde varsa true değerini döndürecektir.
