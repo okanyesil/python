cbfd6dbd34819e843945d181246c64d088caf22f
okan yeşiloğlu <myesiloglu0@gmail.com>
2018-09-23 01:24:45 +0300

    w3school'dan örnekler

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+#w3schooldan çalışırken yaptığım örnekler.
+#pythonda dictionaries küme paranntezi içersinde tanımlanır indekleniir ve değiştirilebilir.
+new_dictionaries={"Marka": "BMW","Model" : "320d","yıl" : "2016"  }
+print(new_dictionaries["Marka"])    
+#dictionary içinde değer aramaya yapmaya yarayan diğer bir method ise get()
+print(new_dictionaries.get("yıl"))
+                  
