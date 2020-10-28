e773ddf033221c9f280c0bf72967e090b908675d
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2020-10-28 13:34:54 +0300

    yüz tanıma

---
+++ yuztanima.py
@@ -0,0 +1,25 @@
+import cv2
+
+#cascade dosyasını yükleme
+
+face_cascade = cv2.CascadeClassifier('face.xml')
+
+#resmi okuma
+
+img = cv2.imread('resim.jpg')
+
+#resmi griye çevirme
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#yüz tanıma
+
+faces = face_cascade.detectMultiScale(gray,1.1,4)
+
+#yüzün çevresinde dikdörtgen çizme
+
+for(x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y),(x+w,y+h),(255,0,0),2)
+#çıktıyı göstermek
+cv2.imshow('img',img)
+cv2.waitKey()
